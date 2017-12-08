0b6d29ab545521e9c0c841af53204a70a91267bb
scwllj <33397888+scwllj@users.noreply.github.com>
2017-12-08 17:58:17 +0800

    aa

--- kuaiqian/app/src/main/java/sihuo/app/com/kuaiqian/utils/X5WebView.java
+++ kuaiqian/app/src/main/java/sihuo/app/com/kuaiqian/utils/X5WebView.java
@@ -2,17 +2,31 @@ package sihuo.app.com.kuaiqian.utils;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.Message;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
+import com.tencent.smtt.export.external.interfaces.JsResult;
+import com.tencent.smtt.export.external.interfaces.WebResourceError;
+import com.tencent.smtt.export.external.interfaces.WebResourceRequest;
+import com.tencent.smtt.export.external.interfaces.WebResourceResponse;
 import com.tencent.smtt.sdk.CookieSyncManager;
 import com.tencent.smtt.sdk.QbSdk;
+import com.tencent.smtt.sdk.ValueCallback;
+import com.tencent.smtt.sdk.WebChromeClient;
 import com.tencent.smtt.sdk.WebSettings;
 import com.tencent.smtt.sdk.WebSettings.LayoutAlgorithm;
 import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
 
 import sihuo.app.com.kuaiqian.R;
 
@@ -32,6 +46,7 @@ public class X5WebView extends WebView {
 		initWebViewSettings();
 		this.getView().setClickable(true);
 		setOnLongClickListener(this);
+		setClient();
 	}
 
 	private void initWebViewSettings() {
@@ -61,6 +76,84 @@ public class X5WebView extends WebView {
 		// this.getSettingsExtension().setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);//extension
 		// settings 的设计
 	}
+
+	void setClient(){
+		this.setWebViewClient(new WebViewClient(){
+
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				try {
+					if(url.startsWith("intent://")){
+						Intent intent = Intent.parseUri(url,Intent.URI_INTENT_SCHEME);
+						intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+						getContext().startActivity(intent);
+						return true;
+					}else if(!url.startsWith("http")){
+						Intent intent = new Intent(Intent.ACTION_VIEW);
+						intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+						intent.setData(Uri.parse(url));
+						getContext().startActivity(intent);
+						return true;
+					}
+					view.loadUrl(url);
+					return true;
+				}catch (Exception e){
+//                    Log.e("----should--error", ""+e.getMessage());
+				}
+				return super.shouldOverrideUrlLoading(view,url);
+
+			}
+
+
+		});
+
+		this.setWebChromeClient(new WebChromeClient(){
+			@Override
+			public boolean onJsAlert(WebView webView, String s, String s1, JsResult jsResult) {
+				jsResult.confirm();
+				return super.onJsAlert(webView, s, s1, jsResult);
+			}
+
+			@Override
+			public boolean onCreateWindow(WebView webView, boolean b, boolean b1, Message message) {
+				NewWindowView newview = (NewWindowView) LayoutInflater.from(getContext()).inflate(R.layout.new_window,null);
+				rootView.addView(newview,new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+				WebView.WebViewTransport transport = (WebView.WebViewTransport) message.obj;//以下的操作应该就是让新的webview去加载对应的url等操作。
+				transport.setWebView(newview.x5WebView);
+				message.sendToTarget();
+				return true;
+			}
+
+			@Override
+			public void onCloseWindow(WebView webView) {
+				super.onCloseWindow(webView);
+			}
+
+
+			@Override
+			public void onProgressChanged(WebView webView, int progress) {
+				super.onProgressChanged(webView, progress);
+				if(getResources().getBoolean(R.bool.pull_refresh_enable)){
+					if(progress==100){
+						refreshLayout.setRefreshing(false);
+					}
+				}
+				if(getResources().getBoolean(R.bool.show_loadng)){
+				}
+			}
+
+			@Override
+			public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> valueCallback, FileChooserParams fileChooserParams) {
+				//            Log.e("----onShowFileChooser", ""+fileChooserParams);
+				uploadMessage = valueCallback;
+				openImageChooserActivity();
+//                return super.onShowFileChooser(webView, valueCallback, fileChooserParams);
+				return true;
+			}
+		});
+	}
+
+
 
 	@Override
 	public boolean onLongClick(View view) {
