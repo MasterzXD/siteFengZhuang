0aefd259faf84f6f67b7471c8b1e23c6cc4f3a25
scwllj <33397888+scwllj@users.noreply.github.com>
2018-04-12 22:28:30 +0800

    a

--- kuaiqian/app/src/main/java/sihuo/app/com/kuaiqian/MyApplication.java
+++ kuaiqian/app/src/main/java/sihuo/app/com/kuaiqian/MyApplication.java
@@ -1,8 +1,13 @@
 package sihuo.app.com.kuaiqian;
 
 import android.app.Application;
+import android.app.Notification;
+
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
+
+import cn.jpush.android.api.BasicPushNotificationBuilder;
+import cn.jpush.android.api.JPushInterface;
 
 
 /**
@@ -16,6 +21,14 @@ public class MyApplication extends Application {
 //        UMConfigure.setLogEnabled(true);
 //        UMConfigure.init(getApplicationContext(), UMConfigure.DEVICE_TYPE_PHONE,"");
 //        MobclickAgent.setScenarioType(getApplicationContext(), MobclickAgent.EScenarioType.E_UM_NORMAL);
+
+        BasicPushNotificationBuilder builder = new BasicPushNotificationBuilder(this);
+        builder.statusBarDrawable = R.drawable.iconx;
+        builder.notificationFlags = Notification.FLAG_AUTO_CANCEL;  //设置为自动消失
+        builder.notificationDefaults = Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS;  // 设置为铃声与震动都要
+        JPushInterface.setPushNotificationBuilder(1, builder);
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this.getApplicationContext());
     }
 
 }
