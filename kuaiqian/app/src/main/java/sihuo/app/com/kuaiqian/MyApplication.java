134f31449de9653ed394cd45837278261e760a77
15881098993@163.com <lj18780119223>
2018-06-29 22:26:46 +0800

    a

--- kuaiqian/app/src/main/java/sihuo/app/com/kuaiqian/MyApplication.java
+++ kuaiqian/app/src/main/java/sihuo/app/com/kuaiqian/MyApplication.java
@@ -1,6 +1,10 @@
 package sihuo.app.com.kuaiqian;
 
 import android.app.Application;
+import android.app.Notification;
+
+import cn.jpush.android.api.BasicPushNotificationBuilder;
+import cn.jpush.android.api.JPushInterface;
 
 
 /**
@@ -11,17 +15,14 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        UMConfigure.setLogEnabled(true);
-//        UMConfigure.init(getApplicationContext(), UMConfigure.DEVICE_TYPE_PHONE,"");
-//        MobclickAgent.setScenarioType(getApplicationContext(), MobclickAgent.EScenarioType.E_UM_NORMAL);
 
-//        BasicPushNotificationBuilder builder = new BasicPushNotificationBuilder(this);
-//        builder.statusBarDrawable = R.drawable.iconx;
-//        builder.notificationFlags = Notification.FLAG_AUTO_CANCEL;  //设置为自动消失
-//        builder.notificationDefaults = Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS;  // 设置为铃声与震动都要
-//        JPushInterface.setPushNotificationBuilder(1, builder);
-//        JPushInterface.setDebugMode(true);
-//        JPushInterface.init(this.getApplicationContext());
+        BasicPushNotificationBuilder builder = new BasicPushNotificationBuilder(this);
+        builder.statusBarDrawable = R.drawable.iconx;
+        builder.notificationFlags = Notification.FLAG_AUTO_CANCEL;  //设置为自动消失
+        builder.notificationDefaults = Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS;  // 设置为铃声与震动都要
+        JPushInterface.setPushNotificationBuilder(1, builder);
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this.getApplicationContext());
     }
 
 }
